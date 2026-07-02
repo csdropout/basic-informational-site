6be4a85747874299f6cf24d1aa7ce62463d20d45
qile <98936712+csdropout@users.noreply.github.com>
2026-07-02 14:49:55 +0800

    Add server logic

---
+++ index.js
@@ -0,0 +1,22 @@
+import http from "node:http";
+import fs from "node:fs";
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "text/html" });
+
+  const url = request.url;
+
+  if (url === "/") {
+    fs.createReadStream("./index.html").pipe(response);
+  } else if (url === "/about") {
+    fs.createReadStream("./about.html").pipe(response);
+  } else if (url === "/contact-me") {
+    fs.createReadStream("./contact-me.html").pipe(response);
+  } else {
+    fs.createReadStream("./404.html").pipe(response);
+  }
+});
+
+server.listen(8080, () => {
+  console.log("Server is running at port 8080");
+});
